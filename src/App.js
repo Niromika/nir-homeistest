d376f46b707051855d9aaa3f66a28fd3b262fc95
Nir Adar <niromika88@gmail.com>
2019-12-18 01:51:46 +0200

    styled finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
 import 'react-accessible-accordion/dist/fancy-example.css';
+import logo from './img/logo.png'
 import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel } from 'react-accessible-accordion';
 
 
 const AppStyled = styled.div`
   display: flex;
+  color: #fff;
+  text-shadow:
+    -1px -1px 0 #000,  
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;
+  }
 
   .accordion {
     max-width: 220px;
-  }
 
-  .accordion__button {
-    width: initial;
+    .accordion__panel {
+      background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .accordion__button {
+      width: initial;
+      background-color: rgba(0, 0, 0, 0.8);
+      color: #fff;
+    }
+
+    .origin-list {
+      list-style-type: none;
+
+      .origin-item {
+        margin: 5px;
+      }
+    }
   }
 
   .content {
     flex: 1;
     text-align: center;
-  }
+    margin-top: 15px;
+    font-size: 25px;
+    letter-spacing: 2px;
 
+    .title {
+      display: inline-block;
+    }
+
+    .logo {
+      width: 40px;
+      position: absolute;
+      right: 0;
+      margin: 40px;
+    }
+  }
 `
 
 class App extends React.Component {
@@ -50,9 +85,9 @@ class App extends React.Component {
                         </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
-                      <ul>
+                      <ul className="origin-list">
                         {country.originUpperLevels.map((originCountry, i) => {
-                          return <li key={i}>{originCountry}</li>
+                          return <li className="origin-item" key={i}>{originCountry}</li>
                         })}
                       </ul>
                     </AccordionItemPanel>
@@ -61,7 +96,8 @@ class App extends React.Component {
             })}
         </Accordion>
         <div className="content">
-          <h1>Homeis Communities</h1>
+          <h1 className="title">Homeis Communities</h1>
+          <img className="logo" src={logo} alt=""/>
         </div>
       </AppStyled>
     );
